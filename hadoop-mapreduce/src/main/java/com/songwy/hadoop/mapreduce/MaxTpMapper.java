7c3e117377afae75a0cd0d50d86221710d79beeb
Ubuntu <wasong@wasongUbuntu.rlvbdlfnozuu1n1qkdgc43cfgh.hx.internal.cloudapp.net>
2019-03-11 13:38:15 +0000

    Add error handling in Mapper

--- hadoop-mapreduce/src/main/java/com/songwy/hadoop/mapreduce/MaxTpMapper.java
+++ hadoop-mapreduce/src/main/java/com/songwy/hadoop/mapreduce/MaxTpMapper.java
@@ -8,6 +8,11 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
 public class MaxTpMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+    private static final int HIGHEST_TEMPERATURE = 1000;
+
+    enum Temperature {
+        OVER_100
+    }
 
     private NcdcRecordParser parser = new NcdcRecordParser();
 
@@ -15,7 +20,14 @@ public class MaxTpMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
     public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         parser.parse(value);
         if (parser.isValidTp()){
-            context.write(new Text(parser.getYear()), new IntWritable(parser.getAirTp()));
+            int Tp = parser.getAirTp();
+            if (Tp > HIGHEST_TEMPERATURE){
+                System.err.println("Temperature over 100 degress for input: " + value);
+                context.setStatus("Detected possibly corrupt record, see log.");
+                context.getCounter(Temperature.OVER_100).increment(1);
+            }
+            context.write(new Text(parser.getYear()), new IntWritable(Tp));
         }
     }
 }
+
